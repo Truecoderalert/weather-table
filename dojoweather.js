595daa436824b063aa98cc5c96a3e8069868fed8
Truecoderalert <141266977+Truecoderalert@users.noreply.github.com>
2024-01-12 16:33:27 -0500

    Add files via upload

---
+++ dojoweather.js
@@ -0,0 +1,21 @@
+function weatheralert(){
+alert('loading weather report')
+weatheralert() = document.querySelector(".topnav")
+weatheralert.style.display = "block";
+}
+
+function wegotcookies(){
+
+wegotcookies = document.querySelector(".acceptbox")
+wegotcookies.style.display = "block";
+}
+
+
+function closedcookiejar(){
+    closedcookiejar= document.querySelector(".acceptbox")
+    closedcookiejar.style.display ="none";
+}
+
+
+
+
